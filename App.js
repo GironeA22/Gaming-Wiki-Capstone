f0e26890606805b93bbf67bb294a04670acfce91
GironeA22 <gironea22@gmail.com>
2022-02-02 18:23:05 -0700

    Forgot to switch branches, working on random background images

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -9,7 +10,6 @@
 import React from 'react';
 // import type {Node} from 'react';
 import {
-  SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
@@ -17,212 +17,91 @@ import {
   useColorScheme,
   View,
   ImageBackground,
-  KeyboardAvoidingView,
-  Platform,
-  ActivityIndicator,
+  // KeyboardAvoidingView, // Automatically pushes elements out from under keyboard
+  // ActivityIndicator, // The spinning loader
 } from 'react-native';
 
 import {
   Colors,
-  DebugInstructions,
+  // DebugInstructions,
   Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+  // LearnMoreLinks,
+  // ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import {getLocationId, getWeather} from './utils/api';
-import getImageForWeather from './utils/getImageForWeather';
-import getIconForWeather from './utils/getIconForWeather';
+// import moment from 'moment';
 
-import moment from 'moment';
+// import SearchInput from './SearchInput';
 
-import SearchInput from './SearchInput';
+import BG from './utils/RandomBG';
 
-// const Section = ({children, title}): Node => {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// };
+const Section = ({ children, title }): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
 
-// const App: () => Node = () => {
-//   const isDarkMode = useColorScheme() === 'dark';
-
-//   const backgroundStyle = {
-//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-//   };
-
-//   return (
-//     <SafeAreaView style={backgroundStyle}>
-//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-//       <ScrollView
-//         contentInsetAdjustmentBehavior="automatic"
-//         style={backgroundStyle}>
-//         <Header />
-//         <View
-//           style={{
-//             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-//           }}>
-//           <Section title="Step One">
-//             Edit <Text style={styles.highlight}>App.js</Text> to change this
-//             screen and then come back to see your edits.
-//           </Section>
-//           <Section title="See Your Changes">
-//             <ReloadInstructions />
-//           </Section>
-//           <Section title="Debug">
-//             <DebugInstructions />
-//           </Section>
-//           <Section title="Learn More">
-//             Read the docs to discover what to do next:
-//           </Section>
-//           <LearnMoreLinks />
-//         </View>
-//       </ScrollView>
-//     </SafeAreaView>
-//   );
-// };
-
-// CLASS
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // bind SCOPE
-    this.handleDate = this.handleDate.bind(this);
-
-    // STATE
-    this.state = {
-      loading: false,
-      error: false,
-
-      location: '',
-      temperature: 0,
-      weather: '',
-      created: '2000-01-01T00:00:00.000000Z'
-    };
-
-  }
-  // Life cycle
-  componentDidMount() {
-    this.handleUpdateLocation('Kiev');
-  }
-
-  // Parse of date
-  handleDate = date => moment(date).format("hh:mm:ss");
-
-  // Update current location
-  handleUpdateLocation = async city => {
-    if (!city) return;
-
-    this.setState({ loading: true }, async () => {
-      try {
-
-        const ID = await getLocationId(city);
-        const { location, weather, temperature, created } = await getWeather(ID);
-
-        this.setState({
-          loading: false,
-          error: false,
-          location,
-          weather,
-          temperature,
-          created,
-        });
-
-
-      } catch (e) {
-
-        this.setState({
-          loading: false,
-          error: true,
-        });
-
-      }
-    });
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+  const image = {
+    uri: './assets/EGS_TheBindingofIsaacRepentance_NicalisIncEdmundMcmillen_S2-1200x1600-eb480826546ffb2ed1560ceec262b615.jpg',
   };
 
-  // RENDERING
-  render() {
-
-    // GET values of state
-    const { loading, error, location, weather, temperature, created } = this.state;
-
-    // Activity
-    return (
-      <KeyboardAvoidingView style={styles.container} behavior="padding">
-
-        <StatusBar barStyle="light-content" />
-
-        <ImageBackground
-          source={getImageForWeather(weather)}
-          style={styles.imageContainer}
-          imageStyle={styles.image}
+  return (
+    <>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+        // style={{
+        //   backgroundColor: isDarkMode ? Colors.black : Colors.white,
+        // }}>
+        // <Section title="Step One">
+        //   Edit <Text style={styles.highlight}>App.js</Text> to change this
+        //   screen and then come back to see your edits.
+        // </Section>
+        // <Section title="See Your Changes">
+        //   <ReloadInstructions />
+        // </Section>
+        // <Section title="Debug">
+        //   <DebugInstructions />
+        // </Section>
+        // <Section title="Learn More">
+        //   Read the docs to discover what to do next:
+        // </Section>
+        // <LearnMoreLinks />
         >
-
-          <View style={styles.detailsContainer}>
-
-            <ActivityIndicator animating={loading} color="white" size="large" />
-
-            {!loading && (
-              <View>
-                {error && (
-                  <Text style={[styles.smallText, styles.textStyle]}>
-                    😞 Could not load your city or weather. Please try again later...
-                  </Text>
-                )}
-                {!error && (
-                  <View>
-                    <Text style={[styles.largeText, styles.textStyle]}>
-                      {getIconForWeather(weather)} {location}
-                    </Text>
-                    <Text style={[styles.smallText, styles.textStyle]}>
-                       {weather}
-                    </Text>
-                    <Text style={[styles.largeText, styles.textStyle]}>
-                      {`${Math.round(temperature)}°`}
-                    </Text>
-                  </View>
-                )}
-
-                <SearchInput
-                  placeholder="Search any city"
-                  onSubmit={this.handleUpdateLocation}
-                />
-
-                {!error && (
-                  <Text style={[styles.smallText, styles.textStyle]}>
-                    Last update: {this.handleDate(created)}
-                  </Text>
-                )}
-
-              </View>
-            )}
-          </View>
-        </ImageBackground>
-      </KeyboardAvoidingView>
-    );
-  }
-}
+          <ImageBackground source={image} style={image} />
+        </View>
+      </ScrollView>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -246,7 +125,6 @@ const styles = StyleSheet.create({
   },
   textStyle: {
     textAlign: 'center',
-    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
     color: 'white',
   },
   largeText: {
@@ -257,4 +135,4 @@ const styles = StyleSheet.create({
   },
 });
 
-// export default App;
+export default App;
