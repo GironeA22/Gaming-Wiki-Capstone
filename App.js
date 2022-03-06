a3d5b639d8e339f6c2e927a90735506bfb756db2
GironeA22 <gironea22@gmail.com>
2022-03-06 11:24:46 -0700

    Ready to merge

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import React, { useState } from 'react';
+import React from 'react';
 
 import {
   ScrollView,
@@ -8,9 +8,19 @@ import {
   Text,
   useColorScheme,
   View,
-  TextInput,
   ImageBackground,
+  Button,
 } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator(
+  //   {
+  //   Home: {screen: Homepage},
+  //   Search: {screen: Searchpage},
+  //   // StatusBar: {screen: StatusBar},
+  // }
+);
 
 import {
   Colors,
@@ -18,84 +28,77 @@ import {
 
 import Homepage from './Pages/Homepage';
 import Minecraft from './assets/jbareham_191158_ply0958_decade_minecraft.jpg';
+import Searchpage from './Pages/Searchpage';
 
-const Section = ({ children, title }): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      {/* <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text> */}
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+// const Section = ({ children, title }): Node => {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   return (
+//     <View style={styles.sectionContainer}>
+//       {/* <Text
+//         style={[
+//           styles.sectionTitle,
+//           {
+//             color: isDarkMode ? Colors.white : Colors.black,
+//           },
+//         ]}>
+//         {title}
+//       </Text> */}
+//       <Text
+//         style={[
+//           styles.sectionDescription,
+//           {
+//             color: isDarkMode ? Colors.light : Colors.dark,
+//           },
+//         ]}>
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// };
 
-const App: () => Node = () => {
+const App: () => Node = ({ navigation }) => {
   const isDarkMode = useColorScheme() === 'dark';
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
+  // const [searchTerm, setSearchTerm] = useState('');
+  // const [searchResults, setSearchResults] = useState([]);
 
-  function MapResults() {
-    fetch('https://minecraft-ids.grahamedgecombe.com/items.json')
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setSearchResults(result.filter(item =>
-            item.name.toLowerCase().includes(searchTerm.toLowerCase()))
-          );
-        }
-      );
-    return (
-      searchResults.map((item, i) => (
-        (searchTerm.toLowerCase() === item.name.toLowerCase()) ? <Text key={i}>{item.name}</Text> : null
-      ))
-    );
-  }
+  // function MapResults() {
+  //   fetch('https://minecraft-ids.grahamedgecombe.com/items.json')
+  //     .then(res => res.json())
+  //     .then(
+  //       (result) => {
+  //         setSearchResults(result.filter(item =>
+  //           item.name.toLowerCase().includes(searchTerm.toLowerCase()))
+  //         );
+  //       }
+  //     );
+  //   return (
+  //     searchResults.map((item, i) => (
+  //       (searchTerm.toLowerCase() === item.name.toLowerCase()) ? <Text key={i}>{item.name}</Text> : null
+  //     ))
+  //   );
+  // }
 
   return (
     <>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <ImageBackground source={Minecraft} resizeMode="cover" style={styles.image}>
-          <View>
-            <TextInput
-              type="text"
-              style={styles.searchBox}
-              placeholder="Search a Minecraft Item (for now)"
-              name="searchMCItems"
-              onChangeText={(text) => setSearchTerm(text)}
-              onSubmitEditing={MapResults}
-            />
-            <Section>
-              <MapResults />
-            </Section>
-            {/* </View> */}
-            {/* <View> */}
-            <Homepage />
-          </View>
-        </ImageBackground>
-      </ScrollView>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={Homepage}>
+        <Stack.Screen name="Home" component={Homepage} />
+        <Stack.Screen name="Search" component={Searchpage} />
+      </Stack.Navigator >
+    </NavigationContainer>
+    {/* <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={backgroundStyle}>
+      <ImageBackground source={Minecraft} resizeMode="cover" style={styles.image}>
+      <View>
+        <Homepage />
+      </View>
+      </ImageBackground>
+    </ScrollView> */}
+</>
   );
 };
 
---
+++ Pages/Searchpage.js
@@ -0,0 +1,112 @@
+/* eslint-disable prettier/prettier */
+
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  StatusBar,
+  ImageBackground,
+  TouchableWithoutFeedback,
+  useColorScheme,
+  ScrollView,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+import React, { useState } from 'react';
+
+import Minecraft from '../assets/jbareham_191158_ply0958_decade_minecraft.jpg';
+
+export default function Searchpage() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const isDarkMode = useColorScheme() === 'dark';
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  function MapResults() {
+    fetch('https://minecraft-ids.grahamedgecombe.com/items.json')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setSearchResults(result.filter(item =>
+            item.name.toLowerCase().includes(searchTerm.toLowerCase()))
+          );
+        }
+      );
+    return (
+      searchResults.map((item, i) => (
+        (searchTerm.toLowerCase() === item.name.toLowerCase()) ? <Text key={i}>{item.name}</Text> : null
+      ))
+    );
+  }
+  return (
+    <>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+         <View>
+         <ImageBackground source={Minecraft} resizeMode="cover" style={styles.image}>
+            <TextInput
+              type="text"
+              style={styles.searchBox}
+              placeholder="Search a Minecraft Item (for now)"
+              name="searchMCItems"
+              onChangeText={(text) => setSearchTerm(text)}
+              onSubmitEditing={MapResults}
+            />
+            <Text style={styles.searchBoxResults}>
+              <TouchableWithoutFeedback>
+              <MapResults />
+              </TouchableWithoutFeedback>
+            </Text>
+          </ImageBackground>
+        </View>
+      </ScrollView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  searchBox: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    opacity: 0.5,
+    borderRadius: 25,
+    height: 50,
+    width: 500,
+    marginLeft: 50,
+    marginTop: 10,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: 'black',
+  },
+  searchBoxResults: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    opacity: 0.5,
+    borderRadius: 25,
+    height: 756,
+    width: 500,
+    marginLeft: 50,
+    marginTop: 10,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: 'black',
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  },
+  textStyle: {
+    textAlign: 'center',
+    color: 'white',
+  },
+});
